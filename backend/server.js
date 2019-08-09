0e481ac337886b8593d97bfe6b9c3014f42c7028
Koobi33 <Koobi@mail.ru>
2019-08-09 09:55:56 +0300

    inserting in mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
 
 // const morgan = require('morgan');
+
 
 
 const server = express();
@@ -19,7 +22,19 @@ server.get('/', (req, res) => {
 server.listen(PORT, ()=> {console.log(`server just starting on ${PORT} port`  + '\n ')});
 
 server.post('/send',async (req, res) => {
-  console.log(req.body);
+
+  mongoClient.connect(function(err, client){
+    const db = client.db("usersdb");
+    const collection = db.collection("users");
+    collection.insertOne(req.body, function(err, result){
+      if(err){
+        return console.log(err);
+      }
+      console.log(result.ops);
+      client.close();
+    });
+  });
+
   res
     .status(200)
     .send({ data: 'ok' })
